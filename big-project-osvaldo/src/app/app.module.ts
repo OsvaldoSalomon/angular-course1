8d5d48568f218c4d6258d90f2e9670c04313773f
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2019-09-28 08:28:00 -0400

    The shopping-list module was created, and the declarations were moved into it.

--- big-project-osvaldo/src/app/app.module.ts
+++ big-project-osvaldo/src/app/app.module.ts
@@ -5,8 +5,6 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import {HeaderComponent} from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import {DropdownDirective} from './shared/dropdown.directive';
 import {ShoppingListService} from './shopping-list/shopping-list.service';
 import {AppRoutingModule} from './app-routing.module';
@@ -22,8 +20,6 @@ import {RecipesModule} from './recipes/recipes.module';
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
     DropdownDirective,
     AuthComponent,
     LoadingSpinnerComponent,
---
+++ big-project-osvaldo/src/app/shopping-list/shopping-list.module.ts
@@ -0,0 +1,10 @@
+import {NgModule} from '@angular/core';
+import {ShoppingListComponent} from './shopping-list.component';
+import {ShoppingEditComponent} from './shopping-edit/shopping-edit.component';
+
+@NgModule({
+  declarations: [ShoppingListComponent, ShoppingEditComponent,]
+})
+export class ShoppingListModule {
+
+}
