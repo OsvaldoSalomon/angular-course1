00d64e9cdf672f41f199c0c81c420faeb752a8f7
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2019-10-07 19:09:48 -0400

    More actions were included in the shopping list reducer and the shopping list action, it's still not finished and fully functional

--- big-project-osvaldo/src/app/shopping-list/store/shopping-list.reducer.ts
+++ big-project-osvaldo/src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,15 +1,28 @@
 import { Ingredient } from '../../shared/ingredient.model';
 import * as ShoppingListActions from './shopping-list.actions';
 
-const initialState = {
+export interface State {
+  ingredients: Ingredient[];
+  editedIngredient: Ingredient;
+  editedIngredientIndex: number;
+
+}
+
+export interface AppState {
+  shoppingList: State
+}
+
+const initialState: State = {
   ingredients: [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
-  ]
+  ],
+  editedIngredient: null,
+  editedIngredientIndex: -1
 };
 
 export function shoppingListReducer(
-  state = initialState,
+  state: State = initialState,
   action: ShoppingListActions.ShoppingListActions
 ) {
   switch (action.type) {
@@ -42,6 +55,18 @@ export function shoppingListReducer(
         ingredients: state.ingredients.filter((ig, igIndex) => {
           return igIndex !== action.payload;
         })
+      };
+    case ShoppingListActions.START_EDIT:
+      return {
+        ...state,
+        editedIngredientIndex: action.payload,
+        editedIngredient: { ...state.ingredients[action.payload]}
+      };
+    case ShoppingListActions.STOP_EDIT:
+      return {
+        ...state,
+        editedIngredient: null,
+        editedIngredientIndex: -1
       };
     default:
       return state;
