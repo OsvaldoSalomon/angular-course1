b2dee7a033c21efa41fc0210a8ec4b54a7c92458
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2019-10-23 06:01:39 -0400

    The autoLogin is fully implemented with ngrx now, and it works just fine

--- big-project-osvaldo/src/app/app.module.ts
+++ big-project-osvaldo/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
-
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -11,6 +11,7 @@ import { SharedModule } from './shared/shared.module';
 import {CoreModule} from './core.module';
 import * as fromApp from './store/app.reducer';
 import {AuthEffects} from './auth/store/auth.effects';
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent,HeaderComponent],
@@ -20,6 +21,7 @@ import {AuthEffects} from './auth/store/auth.effects';
     AppRoutingModule,
     StoreModule.forRoot(fromApp.appReducer),
     EffectsModule.forRoot([AuthEffects]),
+    StoreDevtoolsModule.instrument({ logOnly: environment.production }),
     SharedModule,
     CoreModule
   ],
