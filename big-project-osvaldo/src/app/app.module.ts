65a3ade7f4c76e9f1f64c76fb3f18cec1642354b
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2019-10-24 06:02:57 -0400

    The process for adding ngrx to the recipes has started the store folder has been created as well as the actions, effects and reducer file

--- big-project-osvaldo/src/app/app.module.ts
+++ big-project-osvaldo/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -22,6 +24,7 @@ import {environment} from '../environments/environment';
     StoreModule.forRoot(fromApp.appReducer),
     EffectsModule.forRoot([AuthEffects]),
     StoreDevtoolsModule.instrument({ logOnly: environment.production }),
+    StoreRouterConnectingModule.forRoot(),
     SharedModule,
     CoreModule
   ],
