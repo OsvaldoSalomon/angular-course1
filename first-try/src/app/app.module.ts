1958f7bcbb95a7c6e0f10f2e557b2416878af1cc
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2019-11-05 05:01:34 -0500

    This commit is from yesterday

--- first-try/src/app/app.module.ts
+++ first-try/src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from "./header/header.component";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
@@ -13,4 +16,6 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
