cc93b90c1f2b46c2231167db47bae8e653152038
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2019-11-11 06:01:31 -0500

    I added some files, fixed six errors, just need to complement the media to the app module

--- first-try/src/app/albums/albums-list/album-list.component.ts
+++ first-try/src/app/albums/albums-list/album-list.component.ts
@@ -4,10 +4,10 @@ import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  templateUrl: './album-list.component.html',
+  styleUrls: ['./album-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class AlbumListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
     new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
--- first-try/src/app/app.module.ts
+++ first-try/src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from "./header/header.component";
 import {AlbumsComponent} from "./albums/albums.component";
+import {SongsItemComponent} from "./albums/albums-list/songs-item/songs-item.component";
+import {AlbumListComponent} from "./albums/albums-list/album-list.component";
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AlbumsComponent
+    AlbumsComponent,
+    SongsItemComponent,
+    AlbumListComponent
   ],
   imports: [
     BrowserModule
