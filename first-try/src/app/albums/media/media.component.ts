712a811dca1e000f852b0c6992d6c78fc6f80b5b
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2019-11-12 06:00:56 -0500

    Added the shopping files, I have four errors now

---
+++ first-try/src/app/albums/media/media.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'media-app',
+  templateUrl: './media.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class MediaComponent {}
+
--- first-try/src/app/app.module.ts
+++ first-try/src/app/app.module.ts
@@ -6,6 +6,9 @@ import { HeaderComponent } from "./header/header.component";
 import {AlbumsComponent} from "./albums/albums.component";
 import {SongsItemComponent} from "./albums/albums-list/songs-item/songs-item.component";
 import {AlbumListComponent} from "./albums/albums-list/album-list.component";
+import {MediaComponent} from "./albums/media/media.component";
+import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
+import {ShoppingEditComponent} from "./shopping-list/shopping-edit/shopping-edit.component";
 
 
 @NgModule({
@@ -14,7 +17,10 @@ import {AlbumListComponent} from "./albums/albums-list/album-list.component";
     HeaderComponent,
     AlbumsComponent,
     SongsItemComponent,
-    AlbumListComponent
+    AlbumListComponent,
+    MediaComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
