18d42917d77e1b5578e4c19b66521bb6cfd4d165
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2019-11-17 06:44:02 -0500

    Added the albums preview to the home file, and made some small changes

--- first-try/src/app/home/home.component.ts
+++ first-try/src/app/home/home.component.ts
@@ -1,8 +1,20 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Band } from "./home.model";
 
 @Component({
   selector: 'home-app',
   templateUrl: './home.component.html'
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit{
+
+  albums: Band[] = [
+    new Band('amo', 'Latest album', 'https://images.genius.com/a8ec36c3c37346c199f3895e6c7cd520.600x600x1.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
 
