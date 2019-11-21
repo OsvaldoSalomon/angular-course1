dd495343dc3a94653935288e6a9442d91631f5d5
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2019-11-21 06:01:05 -0500

    Deleted the code that was giving me errors, some of it I moved it to the home component

--- first-try/src/app/home/home.component.ts
+++ first-try/src/app/home/home.component.ts
@@ -6,7 +6,11 @@ import { HomeModel } from "./home.model";
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit{
+  albums: any;
 
+  // albums: BandModel[] = [
+  //   new BandModel('amo', 'Latest album', 'https://images.genius.com/a8ec36c3c37346c199f3895e6c7cd520.600x600x1.jpg')
+  // ];
   // albums: BandModel[] = [
   //   new BandModel('amo', 'Latest album', 'https://images.genius.com/a8ec36c3c37346c199f3895e6c7cd520.600x600x1.jpg')
   // ];
